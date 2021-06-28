3eef8f2d38cf252be42c6e52f788b3e61a1eb058
trantrungta123 <tattse140962@fpt.edu.vn>
2021-06-28 21:18:25 +0700

    testt

---
+++ AppDemo/AppDemo/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace AppDemo
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+            MainPage = new NavigationPage(new PageDangNhap());
+        }
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ AppDemo/AppDemo/Hethong/PageDangNhap.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using System.Net.Http.Headers;
+using System.Net.Http;
+using Newtonsoft.Json;
+using YViet_EMR.Model;
+namespace AppDemo
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PageDangNhap : ContentPage
+	{
+       // View pageContent;
+		public PageDangNhap ()
+		{
+			InitializeComponent ();
+            //NavigationPage.SetHasBackButton(this, false);
+           
+
+           
+           
+        }
+        protected override  void OnAppearing()
+        {
+            base.OnAppearing();
+            
+        }
+        #region Progress
+        View viewContent;
+        public void ShowProgressDisplay()
+        {
+            viewContent = Content;
+            this.IsEnabled = false;
+            var content = Content;
+
+            var grid = new Grid();
+            grid.Children.Add(content);
+            var gridProgress = new Grid { BackgroundColor = Color.FromHex("#64FFE0B2"), Padding = new Thickness(50) };
+            gridProgress.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            gridProgress.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Auto) });
+            gridProgress.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            gridProgress.SetBinding(VisualElement.IsVisibleProperty, "IsWorking");
+            var activity = new ActivityIndicator
+            {
+                IsEnabled = true,
+                IsVisible = true,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                IsRunning = true
+            };
+            gridProgress.Children.Add(activity, 0, 1);
+            grid.Children.Add(gridProgress);
+            Content = grid;
+
+        }
+        void CloseProgressDisplay()
+        {
+            if (viewContent != null) Content = viewContent;
+            viewContent = null;
+            this.IsEnabled = true;
+        }
+        #endregion
+       
+
+          private async void DangNhap_Clicked(object sender, EventArgs e)
+        {
+            // CrossLocalNotifications.Current.Show("DrAI", "Hello");
+
+            //ShowImage();
+            //return;
+            // AddProgressDisplay();
+            ShowProgressDisplay();
+            TData ob = new TData();
+            ob.Ten1 = entry_id.Text;
+            ob.Ten2 = entry_pass.Text;
+            string url = MainModel.BaseURL + "api/v2/DangNhap/LoginForPatient";
+            var kq = MainModel. PostDataAsyncOb<TResult>(url, ob);
+            object ketqua = await kq;
+            bool dangnhap = false;
+            if (ketqua != null)
+            {
+                TResult t = (TResult)ketqua;
+                dangnhap = t.ThanhCong;
+            }
+
+            //var v = MainModel.BenhNhanDangNhap(entry_id.Text, entry_pass.Text);
+          
+            //bool dangnhap = await v;
+            if (dangnhap)
+            {
+
+                var tb = DisplayAlert("Alert", "Kết quả", "Thanh cong");
+                await tb;
+                CloseProgressDisplay();
+                //await saveGiaoDien;
+                //var showHome = Navigation.PushAsync(new PageHome(HubsHelperKeys.KeysHT_GiaoDien.ToList()));
+
+                //await showHome;
+
+            }
+            else {
+                var tb = DisplayAlert("Alert", "Kết quả", "Thất bại");
+                await tb;
+                CloseProgressDisplay();
+               // Content = pageContent;
+            }
+
+            
+
+        }
+
+      
+
+       }
+}
